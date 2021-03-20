547a2c961da302b7056c948285df73592a991a64
Greymaster <rytbait101204@gmail.com>
2021-03-20 14:35:52 +0300

    Создан: work_base_data.py Добавлено в design_project.py: Создание окна программы PyQt5

---
+++ design_project.py
@@ -0,0 +1,30 @@
+import sys
+
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLineEdit, QLabel, QTextEdit
+from PyQt5.QtGui import QPixmap, QFont, QIcon
+
+class Constant:
+    NAME_APPLICATIONS = "Работа с текстом"
+
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(700, 700, 700, 700)
+        self.setWindowTitle(Constant.NAME_APPLICATIONS)
+
+
+def except_hook(cls, exception, traceback):  # если произойдет ошибка то Pyqt5 не будет замалчивать её
+    sys.__excepthook__(cls, exception, traceback)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    app.setWindowIcon(QIcon("photo/game_icon.png"))
+    ex = Example()
+    ex.show()
+    sys.excepthook = except_hook
+    sys.exit(app.exec())
