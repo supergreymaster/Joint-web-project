073ea87b884db7f8ce1aeb915a34cb875d809ac6
KsoKsero <Ksoksero@yandex.ru>
2021-04-11 13:46:59 +0300

    Рабочая версия функции, которая принимает текст и записывает его в mp3.

--- text_voice.py
+++ text_voice.py
@@ -2,11 +2,11 @@ from gtts import gTTS
 from errors_module_for_text_voice import *
 
 
-def voice_text(text, lang):
+def take_text(text, lang):
     if text:
         voice = gTTS(text, lang=lang)
         voice.save('voice_text.mp3')
-        """Пока я писал этот код, я ориентировался на то, что файлов с озвучкой текста всегда будет один.
+        """Пока я писал этот код, я ориентировался на то, что файл с озвучкой текста всегда будет один.
             Если это не так, и подразумевается, что таких файлов будет много, то я попробую что-то сделать с
             проблемой названия файла."""
     else:
