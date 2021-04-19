1ceef56064ee2489b217fac0e008e3c74a6dd7b4
KsoKsero <Ksoksero@yandex.ru>
2021-04-19 16:53:30 +0300

    Теперь полностью рабочая озвучка текста

--- text_voice.py
+++ text_voice.py
@@ -1,14 +1,23 @@
 from gtts import gTTS
+import pyttsx3
 from errors_module_for_text_voice import *
 
 
-def take_text(text, lang):
+def save_text_as_mp3(text, file_name, lang):
     if text:
         voice = gTTS(text, lang=lang)
-        voice.save('voice_text.mp3')
-        """Пока я писал этот код, я ориентировался на то, что файл с озвучкой текста всегда будет один.
-            Если это не так, и подразумевается, что таких файлов будет много, то я попробую что-то сделать с
-            проблемой названия файла."""
+        voice.save(file_name)
     else:
         raise TextInvalidValue
 
+
+def play_text(text):
+    engine = pyttsx3.init()
+    engine.setProperty('rate', 150)
+    engine.setProperty('volume', 1)
+    engine.say(text)
+    engine.runAndWait()
+
+
+save_text_as_mp3('Советский союз нерушимых республик!', 'test_voice_1.mp3', 'ru')
+play_text('Карл у Клары украл кораллы, Клара у Карла украла кларнет.')
